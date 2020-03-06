2591fa3b492e055c4985126b640888b1d221f066
ramandeep0105 <ramanbalu902@gmail.com>
2020-03-05 21:53:54 -0500

    Add project files.

---
+++ PhoneApplication/Models/Manufacturer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.ComponentModel.DataAnnotations;
+
+using System.Linq;
+
+using System.Threading.Tasks;
+
+
+
+namespace WebApplicationCore.Models
+
+{
+
+    public class Manufacturer
+
+    {
+
+        public Manufacturer()
+
+        {
+
+            this.Founded = DateTime.Now;
+
+            this.ManufacturerName = "";
+
+            this.URL = "";
+
+            this.PhoneList = new List<Phone>();
+
+        }
+
+        [Key]
+
+        public int ManufacturerID { get; set; }
+
+        public string ManufacturerName { get; set; }
+
+        public DateTime Founded { get; set; }
+
+        public string URL { get; set; }
+
+        public virtual ICollection<Phone> PhoneList { get; set; }
+
+    }
+
+}
